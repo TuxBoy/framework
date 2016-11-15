cbfc0a46a22241e97568c82cf8c030917c850a5b
Sébastien Meudec <sebastien.meudec@bappli.com>
2016-11-15 08:57:15 +0100

    78651 added validation on components

--- widget/validate/Result.php
+++ widget/validate/Result.php
@@ -13,11 +13,23 @@ abstract class Result
 	//----------------------------------------------------------------------------------- INFORMATION
 	const INFORMATION = 'information';
 
+	//----------------------------------------------------------------------------------- ARE_INVALID
+	/**
+	 * List of results that means valid. Order is important
+	 */
+	const INVALID_RESULTS = [self::WARNING, self::ERROR];
+
 	//------------------------------------------------------------------------------------------ NONE
 	const NONE = null;
 
 	//----------------------------------------------------------------------------------------- VALID
 	const VALID = true;
+
+	//------------------------------------------------------------------------------------- ARE_VALID
+	/**
+	 * List of results that means valid. Order is important
+	 */
+	const VALID_RESULTS = [self::VALID, self::NONE, self::INFORMATION];
 
 	//--------------------------------------------------------------------------------------- WARNING
 	const WARNING = 'warning';
@@ -30,7 +42,7 @@ abstract class Result
 	 */
 	public static function andResult($result, $and_result)
 	{
-		$levels = [true, null, self::INFORMATION, self::WARNING, self::ERROR];
+		$levels = array_merge(self::VALID_RESULTS, self::INVALID_RESULTS);
 		$result_level     = array_search($result, $levels, true);
 		$and_result_level = array_search($and_result, $levels, true);
 		return ($result_level > $and_result_level) ? $result : $and_result;
@@ -45,8 +57,8 @@ abstract class Result
 	public static function isValid($result, $warning_is_valid = false)
 	{
 		return $warning_is_valid
-			? ($result !== self::ERROR)
-			: !in_array($result, [self::ERROR, self::WARNING], true);
+			? in_array($result, array_merge(self::VALID_RESULTS, [self::WARNING]), true)
+			: in_array($result, self::VALID_RESULTS, true);
 	}
 
 }
