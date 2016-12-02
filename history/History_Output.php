b80411d4e534a3199bc321e7333a0b31cc8de3c8
Sébastien Meudec <sebastien.meudec@bappli.com>
2016-12-02 12:42:40 +0100

    79561 Implements generic history manager, writer and output

---
+++ history/History_Output.php
@@ -0,0 +1,119 @@
+<?php
+namespace ITRocks\Framework\History;
+
+use ITRocks\Framework\Controller\Parameter;
+use ITRocks\Framework\Controller\Parameters;
+use ITRocks\Framework\Dao;
+use ITRocks\Framework\History;
+use ITRocks\Framework\Html\Parser;
+use ITRocks\Framework\Locale\Loc;
+use ITRocks\Framework\Reflection\Annotation\Property\User_Annotation;
+use ITRocks\Framework\Reflection\Reflection_Property;
+use ITRocks\Framework\Tools\Date_Time;
+use ITRocks\Framework\View;
+
+/**
+ * History output specific properties and methods
+ */
+trait History_Output
+{
+
+	//---------------------------------------------------------------------------------------- output
+	/**
+	 * @param $result     string
+	 * @param $parameters Parameters
+	 * @param $class_name string
+	 */
+	public function output(&$result, Parameters $parameters, $class_name)
+	{
+		$object = $parameters->getMainObject();
+		$parameters->set(Parameter::IS_INCLUDED, true);
+		$parameters->set('history_tree', $this->historyTreeByUserDate($object, $class_name));
+		$after = View::run($parameters->getObjects(), [], [], $class_name, 'history');
+		if (strpos($result, 'id="main"')) {
+			$parser = new Parser($result);
+			$parser->merge('section#main', $after);
+			$result = $parser->buffer;
+		}
+		else {
+			$result .= $after;
+		}
+	}
+
+	//------------------------------------------------------------------------- historyTreeByUserDate
+	/**
+	 * Returns repair history, in a ready-for-display ordered and optimized tree
+	 * This is used for user display :
+	 * - properties that have @user hidden or @user invisible will be removed from this tree
+	 *
+	 * @param $object     object the main object on which we have history
+	 * @param $class_name string the history entries class name
+	 * @return array [$id_user] => ['history' => [$date => [History]], 'user' => $user]
+	 */
+	private function historyTreeByUserDate($object, $class_name)
+	{
+		$object_class_name = get_class($object);
+		$property_name = call_user_func([$class_name, 'getObjectPropertyName']);
+		/** @noinspection PhpUndefinedMethodInspection */
+		$history_entries = Dao::search(
+			[$property_name => Dao::getObjectIdentifier($object)],
+			$class_name
+		);
+
+		/** @noinspection PhpUsageOfSilenceOperatorInspection
+		 * Patch to avoid the 'Array was modified by the user comparison function' warning */
+		@usort($history_entries, function(History $a, History $b) {
+			$date_a = substr($a->date->toISO(), 0, 16);
+			$date_b = substr($b->date->toISO(), 0, 16);
+			$compare = ($date_a === $date_b)
+				? (Dao::getObjectIdentifier($a) - Dao::getObjectIdentifier($b))
+				: $a->date->diff($b->date)->compare();
+			return $compare;
+		});
+		$history_tree = [];
+		$key = -1;
+		$user = null;
+		foreach ($history_entries as $history_entry) {
+			if (!Manager::isToBeHistorized($object_class_name, $history_entry->property_name)) {
+				continue;
+			}
+			$property = new Reflection_Property($object_class_name, $history_entry->property_name);
+			$user_annotation = $property
+				? $property->getListAnnotation(User_Annotation::ANNOTATION) : null;
+			if (!$user_annotation || (
+				!$user_annotation->has(User_Annotation::HIDDEN)
+				&& !$user_annotation->has(User_Annotation::INVISIBLE)
+			)) {
+				$this->translateHistoryEntry($property, $history_entry);
+				if (!Dao::is($history_entry->user, $user)) {
+					$key++;
+					$user = $history_entry->user;
+					$history_tree[$key] = ['user' => $user];
+				}
+				$date_time = substr($history_entry->date->toISO(), 0, 16);
+				$history_tree[$key]['history'][$date_time][] = $history_entry;
+			}
+		}
+		return $history_tree;
+	}
+
+	//------------------------------------------------------------------------- translateHistoryEntry
+	/**
+	 * @param $property Reflection_Property
+	 * @param $history  History the history entry
+	 */
+	private function translateHistoryEntry($property, History $history)
+	{
+		if ($property->getType()->isDateTime()) {
+			$history->new_value = $history->new_value ? new Date_Time($history->new_value) : null;
+			$history->old_value = $history->old_value ? new Date_Time($history->old_value) : null;
+		}
+		if ($history->new_value) {
+			$history->new_value = Loc::propertyToLocale($property, $history->new_value);
+		}
+		if ($history->old_value) {
+			$history->old_value = Loc::propertyToLocale($property, $history->old_value);
+		}
+	}
+
+}
---
+++ history/prototype/History.php
@@ -0,0 +1,40 @@
+<?php
+/**/namespace ITRocks\Framework\History\Prototype;/**/
+
+/**
+ * This file is a dynamically generated file and is not part of project sources
+ */
+/**##REMOVE
+ * This file is intended to serve as a prototype source file to generate dynamic classes
+ * We keep it as a regular php file to keep advantages of project editor's behavior like refactoring
+ * This file (a generated one) will not fulfill PSR rules.
+ *
+ * Any occurrence of "main_object_example" (case insensible) will be replaced by the true
+ * main object class name.
+ ##*/
+
+use ITRocks\Framework;
+
+/**
+ * main_object_example's history
+ *
+ * @override object @store false
+ * @override object @var Main_Object_Example
+ * @set Main_object_example_History
+ */
+class History extends Framework\History
+{
+
+	//-------------------------------------------------------------------------- OBJECT_PROPERTY_NAME
+	const OBJECT_PROPERTY_NAME = 'main_object_example';
+
+	//-------------------------------------------------------------------------- $main_object_example
+	/**
+	 * @link Object
+	 * @mandatory
+	 * @replaces object
+	 * @var Main_Object_Example
+	 */
+	public $main_object_example;
+
+}
